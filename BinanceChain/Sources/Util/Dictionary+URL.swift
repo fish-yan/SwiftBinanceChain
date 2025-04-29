3938eb9f9590829ece9e65d3c59c83a44d702d77
yan <15316488+fish-yan@users.noreply.github.com>
2025-04-29 11:53:07 +0800

    update：package

--- BinanceChain/Sources/Util/Dictionary+URL.swift
+++ BinanceChain/Sources/Util/Dictionary+URL.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Dictionary where Key == String, Value == Any {
+extension Dictionary where Key == String, Value == any Any & Sendable {
 
     var query: String {
         let items: [URLQueryItem] = self.compactMap { URLQueryItem(name: $0.key, value: String(describing: $0.value)) }
