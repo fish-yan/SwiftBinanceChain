522edfbb2a35833d5399682017eed4ad4ccaa0ce
yan <15316488+fish-yan@users.noreply.github.com>
2025-04-29 16:42:08 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -12,13 +12,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.4.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.8.4"),
-        .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", exact: "0.19.0"),
-        .package(url: "https://github.com/fish-yan/HDWalletKit.git", branch: "main")
+        .package(url: "https://github.com/fish-yan/HDWalletKit.git", branch: "onto")
     ],
     targets: [
         .target(name: "binancechain",
@@ -26,10 +24,8 @@ let package = Package(
                     .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                     "Alamofire",
                     "SwiftyJSON",
-                    "Starscream",
                     "HDWalletKit",
                     "XCGLogger",
-                    .product(name: "secp256k1", package: "swift-secp256k1"),
                     "SwiftDate",
                     "CryptoSwift"
                 ],
